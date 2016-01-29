9abbec854a3c3b635bebb815d8e939cafc95f031
YoungJoo Lee <youngker@gmail.com>
2016-01-29 21:48:47 +0900

    remove set-selection-coding-system for w32 problem

--- init.el
+++ init.el
@@ -137,7 +137,6 @@
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
 
