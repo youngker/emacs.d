830237b6730cc264f43e9dd4c9ae73ab46d01226
YoungJoo Lee <youngker@gmail.com>
2016-01-30 23:46:01 +0900

    remove font

--- init.el
+++ init.el
@@ -39,8 +39,12 @@
 
 (custom-theme-set-faces
  'default-black
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-t*hrough nil :overline nil :underline nil :slant normal :weight normal :width normal :height 105))))
- '(highlight ((((class color) (min-colors 88) (background dark)) (:background "#111111"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil
+                :background "#000000" :foreground "#ffffff" :strike-t*hrough nil
+                :overline nil :underline nil :slant normal :weight normal
+                :width normal :height 200 :family "Monaco"))))
+ '(highlight ((((class color) (min-colors 88) (background dark))
+               (:background "#111111"))))
  '(region ((nil (:background "#464740"))))
  '(hl-line ((nil (:background "#222222"))))
  '(yas-field-highlight-face ((nil (:background "#333399"))))
@@ -49,7 +53,7 @@
  '(show-paren-match ((nil (:background "#333399"))))
  '(show-paren-mismatch ((((class color)) (:background "#cc1b00"))))
  '(eval-sexp-fu-flash ((t (:background "#000000" :foreground "#333399"))))
- '(helm-source-header ((t (:background "#000000" :foreground "#ff7f24" :bold nil))))
+ '(helm-source-header ((t (:background "#000000" :foreground "#ff7f24"))))
  '(helm-candidate-number ((t (:background nil :foreground "#87cefa" :bold t))))
  '(helm-selection ((t (:background "#222222"))))
  '(helm-visible-mark ((t (:background "#444444" :forground "#ffffff"))))
@@ -121,15 +125,6 @@
 (show-paren-mode +1)
 (winner-mode +1)
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-;; font
-(cond
- ((eq window-system 'x)
-  (set-face-attribute 'default nil :height 105 :bold t :family "DejaVu Sans Mono"))
- ((eq window-system 'w32)
-  (set-face-attribute 'default nil :height 110 :bold t :family "Consolas"))
- ((memq window-system '(ns mac))
-  (set-face-attribute 'default nil :height 140 :family "Monaco")))
 
 ;; locale
 (set-language-environment "Korean")
@@ -207,8 +202,8 @@
 
 (use-package uniquify
   :ensure nil
-  :after ido
-  :config
+  :defer t
+  :init
   (setq uniquify-buffer-name-style 'reverse
         uniquify-separator " • "
         uniquify-after-kill-buffer-p t
@@ -689,6 +684,77 @@
   (dolist (hook elisp-mode-hook-list)
     (add-hook hook #'lisp-mode-setup-hook)
     (add-hook hook #'elisp-mode-setup-hook)))
+
+(eval-after-load "lisp-mode"
+  '(defun lisp-indent-function (indent-point state)
+     "This function is the normal value of the variable `lisp-indent-function'.
+The function `calculate-lisp-indent' calls this to determine
+if the arguments of a Lisp function call should be indented specially.
+INDENT-POINT is the position at which the line being indented begins.
+Point is located at the point to indent under (for default indentation);
+STATE is the `parse-partial-sexp' state for that position.
+If the current line is in a call to a Lisp function that has a non-nil
+property `lisp-indent-function' (or the deprecated `lisp-indent-hook'),
+it specifies how to indent.  The property value can be:
+* `defun', meaning indent `defun'-style
+  \(this is also the case if there is no property and the function
+  has a name that begins with \"def\", and three or more arguments);
+* an integer N, meaning indent the first N arguments specially
+  (like ordinary function arguments), and then indent any further
+  arguments like a body;
+* a function to call that returns the indentation (or nil).
+  `lisp-indent-function' calls this function with the same two arguments
+  that it itself received.
+This function returns either the indentation to use, or nil if the
+Lisp function does not specify a special indentation."
+     (let ((normal-indent (current-column))
+           (orig-point (point)))
+       (goto-char (1+ (elt state 1)))
+       (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+       (cond
+        ;; car of form doesn't seem to be a symbol, or is a keyword
+        ((and (elt state 2)
+              (or (not (looking-at "\\sw\\|\\s_"))
+                  (looking-at ":")))
+         (if (not (> (save-excursion (forward-line 1) (point))
+                     calculate-lisp-indent-last-sexp))
+             (progn (goto-char calculate-lisp-indent-last-sexp)
+                    (beginning-of-line)
+                    (parse-partial-sexp (point)
+                                        calculate-lisp-indent-last-sexp 0 t)))
+         ;; Indent under the list or under the first sexp on the same
+         ;; line as calculate-lisp-indent-last-sexp.  Note that first
+         ;; thing on that line has to be complete sexp since we are
+         ;; inside the innermost containing sexp.
+         (backward-prefix-chars)
+         (current-column))
+        ((and (save-excursion
+                (goto-char indent-point)
+                (skip-syntax-forward " ")
+                (not (looking-at ":")))
+              (save-excursion
+                (goto-char orig-point)
+                (looking-at ":")))
+         (save-excursion
+           (goto-char (+ 2 (elt state 1)))
+           (current-column)))
+        (t
+         (let ((function (buffer-substring (point)
+                                           (progn (forward-sexp 1) (point))))
+               method)
+           (setq method (or (function-get (intern-soft function)
+                                          'lisp-indent-function)
+                            (get (intern-soft function) 'lisp-indent-hook)))
+           (cond ((or (eq method 'defun)
+                      (and (null method)
+                           (> (length function) 3)
+                           (string-match "\\`def" function)))
+                  (lisp-indent-defform state indent-point))
+                 ((integerp method)
+                  (lisp-indent-specform method state
+                                        indent-point normal-indent))
+                 (method
+                  (funcall method indent-point state)))))))))
 
 
 ;; C
