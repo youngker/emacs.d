695e7a5e292254ac536fd645c2968e1f5aafa11a
YoungJoo Lee <youngker@gmail.com>
2023-12-22 23:53:03 +0900

    remove key (C-c .) from c-ts-mode-map

--- packages/setup-ts.el
+++ packages/setup-ts.el
@@ -4,9 +4,12 @@
 
 (use-package treesit-auto
   :defines
-  (treesit-auto-install)
+  (treesit-auto-install
+   c-ts-mode-map)
   :functions
   (global-treesit-auto-mode)
+  :bind (:map c-ts-mode-map
+         ("C-c ." . nil))
   :config
   (setq treesit-auto-install 'prompt)
   (global-treesit-auto-mode))
