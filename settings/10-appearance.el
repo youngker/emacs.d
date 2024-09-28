f1b08f03ec52bb8362f685f7938e029a25a04b5c
YoungJoo Lee <youngker@gmail.com>
2024-09-28 12:27:45 +0900

    update appearance

--- settings/10-appearance.el
+++ settings/10-appearance.el
@@ -2,16 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
 (load-theme 'nord)
 
-(set-face-attribute 'default nil :font "Monaco 16")
+(add-to-list 'default-frame-alist '(font . "Monaco 16"))
+(add-to-list 'default-frame-alist '(fullscreen . fullboth))
+
+(add-hook 'after-make-frame-functions
+          (lambda (frame)
+            (with-selected-frame frame
+              (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+              (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+              (if (fboundp 'tool-bar-mode) (tool-bar-mode -1)))))
 
 (blink-cursor-mode -1)
 (global-font-lock-mode +1)
